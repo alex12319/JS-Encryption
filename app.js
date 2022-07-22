45c071c32bf88f158ac46c12c2cfeca7f7b3390e
alex12319 <alexbrata10@gmail.com>
2022-07-22 09:43:10 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const messageContainer = document.querySelector('.message-input');
 const codeContainer = document.querySelector('.code-input');
 const resultContainer = document.querySelector('.result-input');
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('.btns button');
+const resultMessage = document.querySelector('.result-message');
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'", "_", "-", "&", "@", "#", "$", "%", "*", "(", ")", " "];
-console.log(characters[characters.indexOf(' ') + characters.indexOf('A') - characters.length])
+const copyBtn = document.querySelector('.copy');
 
 btns.forEach(btn =>{
     btn.addEventListener('click',function(e){
@@ -12,8 +13,10 @@ btns.forEach(btn =>{
         if(message && secretKey){
             if(e.target.classList.contains('encrypt')){
                 resultContainer.textContent = encrypt(message,secretKey).toLowerCase();
+                resultMessage.textContent = 'Your encrypted message:';
             }else{
                 resultContainer.textContent = decrypt(message,secretKey).toLowerCase();
+                resultMessage.textContent = 'Your decrypted message:';
             }
             
         }
@@ -21,58 +24,23 @@ btns.forEach(btn =>{
 })
 
 function encrypt(message,secretKey){
-    if(message.length === secretKey.length || message.length < secretKey.length){
         let result = [...message].map((item,index) =>{
-        if(characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[index]) > characters.length){
-                const encryptedCharacter = characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[index]) - characters.length;
-                return characters[encryptedCharacter];
-        }
-          const encryptedCharacter = characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[index]);
+          const encryptedCharacter = (characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[index % secretKey.length])) % characters.length;
           return characters[encryptedCharacter];
         })
         return result.join('');
-    }
-        let i = 0;
-        let result = [...message].map((item) =>{
-            if(i >= secretKey.length) i = 0;
-            if(characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[i]) >= characters.length){
-                const encryptedCharacter = characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[i]) - characters.length;
-                i++;
-              
-                return characters[encryptedCharacter];
-            }
-            const encryptedCharacter = characters.indexOf(item.toUpperCase()) + characters.indexOf(secretKey.toUpperCase()[i]);
-            console.log(encryptedCharacter)
-            i++;
-            return characters[encryptedCharacter];
-          })
-          return result.join('');
 }
 
 function decrypt(message,secretKey){
-    if(message.length === secretKey.length || message.length < secretKey.length){
         let result = [...message].map((item,index) =>{
-            if(characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[index]) < 0){
-                const encryptedCharacter = characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[index]) + characters.length;
-            
-                return characters[encryptedCharacter];
-            }
-          const encryptedCharacter = characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[index]);
+          const encryptedCharacter = (characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[index % secretKey.length]) % characters.length + characters.length) % characters.length;
           return characters[encryptedCharacter];
         })
         return result.join('');
-    }
-        let i = 0;
-        let result = [...message].map((item) =>{
-            if(i >= secretKey.length) i = 0;
-            if(characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[i]) < 0){
-                const encryptedCharacter = characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[i]) + characters.length;
-                i++;
-                return characters[encryptedCharacter];
-            }
-            const encryptedCharacter = characters.indexOf(item.toUpperCase()) - characters.indexOf(secretKey.toUpperCase()[i]);
-            i++;
-            return characters[encryptedCharacter];
-          })
-          return result.join('');
 }
+
+
+copyBtn.addEventListener('click',function(){
+    document.querySelector('textarea').select();
+    document.execCommand('copy');
+})
